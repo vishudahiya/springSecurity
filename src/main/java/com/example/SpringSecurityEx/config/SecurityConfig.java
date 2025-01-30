dc76776d1ea5f450f9491b20f5769dc0f1595702
vishudahiya <vishaldahiya12@gmail.com>
2025-01-31 00:46:23 +0530

    H2 Database Implemented

--- src/main/java/com/example/SpringSecurityEx/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityEx/config/SecurityConfig.java
@@ -1,46 +1,78 @@
 package com.example.SpringSecurityEx.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
+    // ** Custom Filer Chain
     @Bean
     SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
-
+        http.authorizeHttpRequests(requests -> requests.requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
         //Used for creation stateless API, cookies won't be available in this
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
+
+    // **In Memory Authentication
     @Bean
     public UserDetailsService userDetailsService(){
 
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}pass")
+//                .password("{noop}pass")
+                .password(passwordEncoder().encode("pass"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}adminPass")
+//                .password("{noop}adminPass")  noop is used to tell that no encoding is done, used before using PasswordEncoder
+                .password(passwordEncoder().encode("adminPass"))
                 .roles("ADMIN")
                 .build();
 
+//        return new InMemoryUserDetailsManager(user1, admin);
 
-        return new InMemoryUserDetailsManager(user1, admin);
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+
+        return userDetailsManager;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/SpringSecurityEx/controller/GreetingsController.java
+++ src/main/java/com/example/SpringSecurityEx/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecurityEx.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,17 @@ public class GreetingsController {
     public String hello(){
         return "Hello";
     }
+
+    @PreAuthorize("hasRole('USER')")   // Used for Role Based Access
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")   // Used for Role Based Access
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello Admin!";
+    }
+
 }
