84ae73a06b41f85434810b944e2c7627bcec5264
vishudahiya <vishaldahiya12@gmail.com>
2025-01-30 23:43:52 +0530

    Security Setup

---
+++ src/main/java/com/example/SpringSecurityEx/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.SpringSecurityEx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+
+        //Used for creation stateless API, cookies won't be available in this
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}pass")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPass")
+                .roles("ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+}
---
+++ src/test/java/com/example/SpringSecurityEx/SpringSecurityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
